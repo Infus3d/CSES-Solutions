185595933a0cfcdcc3917317bb1504148cf90d3f
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-05-15 12:59:53 -0500

    added more problems to DP

---
+++ cses_solutions/Dynamic Programming/RemovalGame.cpp
@@ -0,0 +1,44 @@
+/* 
+ * Problem link : https://cses.fi/problemset/task/1097
+ * 
+ * You can find a thorough explanation here:
+ * https://codeforces.com/blog/entry/70018
+ * 
+ * Scroll down to locate the problem.
+ * */
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+
+const int N = 5001;
+
+int n;
+ll dp[N][N], x[N], sum;
+
+ll rec(int l, int r){
+  if(l == r) 
+    return x[l];
+  
+  ll &ret = dp[l][r];
+  if(ret != -1LL) return ret;
+  
+  ll leftEnd = x[l] - rec(l+1, r);
+  ll rightEnd = x[r] - rec(l, r-1);
+  ret = max(leftEnd, rightEnd);
+  
+  return ret;
+}
+
+int main(){
+  scanf("%d", &n);
+  for(int i=0; i<n; i++){
+    scanf("%lld", &x[i]);
+    sum += x[i];
+  }
+  
+  memset(dp, -1LL, sizeof(dp));
+  printf("%lld\n", (sum + rec(0, n-1))/2LL);
+  
+  return 0;
+}
---
+++ cses_solutions/Dynamic Programming/TwoSetsII.cpp
@@ -0,0 +1,47 @@
+/* 
+ * Problem link : https://cses.fi/problemset/task/1093
+ * 
+ * When we divide the numbers into two equal sum sets,
+ * the sum of each set will be the half of total, where
+ * total is n * (n+1) / 2.
+ * 
+ * This reduces the problem to: given numbers 1 through n
+ * count how many ways you can achieve total/2 sum by using
+ * each number no more than once.
+ * The reduced problem is a 0-1 knapsack where the outer loop
+ * iterates over the values we want to put into the set and
+ * the inner loop iterates over the sums we have so far in our dp.
+ * By going from maximum sum to minimum sum ensures that we use
+ * each value once.
+ * */
+#include "bits/stdc++.h"
+
+using namespace std;
+
+const int MOD = 1e9+7;
+
+int n, dp[(500 * 501)/2];
+
+int main(){
+  scanf("%d", &n);
+  int totalSum = n * (n+1) / 2;
+  int halfSum = totalSum / 2;
+  
+  dp[0] = 1;
+  for(int j=1; j<n; j++){
+    /* going from halfsum to 0 ensures we use each j once.
+     * To see this, try modifying the loop from 0 to halfSum
+     * and observe the behavior.
+     * */
+    for(int i=halfSum; i >= 0; i--)
+      if(i + j <= halfSum)
+        dp[i + j] = (dp[i + j] + dp[i]) % MOD;
+  }
+  
+  if(totalSum % 2 == 1) //we can't divide into two equal sums when odd
+    printf("0\n");
+  else
+    printf("%d\n", dp[halfSum]); 
+  
+  return 0;
+}
