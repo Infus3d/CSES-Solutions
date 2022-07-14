12502fefaa0150ab93ae8b7f13278d37c77ea02e
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-07-14 10:46:00 -0500

    Update RangeXorQueries.cpp

--- cses_solutions/Range Queries/RangeXorQueries.cpp
+++ cses_solutions/Range Queries/RangeXorQueries.cpp
@@ -6,22 +6,23 @@
  * T[parent] = T[child1] ^ T[child2]
  * ----> The character '^' is the xor operator
  * 
- * However there is special property of xor that makes segment trees
- * reduntant:
+ * However there are special properties of xor that makes segment trees
+ * reduntant.
  * Xor is associative ---> a ^ (b ^ c) = (a ^ b) ^ c
  * Xor is commutative ---> a ^ b = b ^ a
  * Xor of the value with itself is 0 ----> a ^ a = 0
  * Xor of any integer with 0 is itself ----> a ^ 0 = a
  * 
  * Now, combining all these lets us understand: a ^ b ^ a = b
+ *
  * The properties above hints that we can take xor's of the range just like we did with sums!
- * That is, if we want xor of the range [a, b] we just need (xor[1, b] - xor[1, a-1]). Because:
+ * That is, if we want xor of the range [a, b] we just need (xor[1, b] ^ xor[1, a-1]). Because:
  * 
  * xor[1, b]   : xor[1, 2, 3, ... , a-1, a, a+1, ..., b-1, b]
  * xor[1, a-1] : xor[1, 2, 3, ... , a-1]
  * 
- * Taking xor nullifies the range [1, a-1] and leaves us with only [a, b].
- * We can calculate the prefix-xors before all queries and answer them in O(1)
+ * Taking xor of the intervals nullifies the range [1, a-1] and leaves us with only [a, b].
+ * We can calculate the prefix-xors before all queries and answer them each in O(1)
  * 
  * Runtime O(n + q)
  * */
