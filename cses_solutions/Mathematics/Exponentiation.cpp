62a3586d224163f286380ea40a5a9a3bcc935aea
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-07-20 23:30:13 -0500

    Update Exponentiation.cpp

--- cses_solutions/Mathematics/Exponentiation.cpp
+++ cses_solutions/Mathematics/Exponentiation.cpp
@@ -2,14 +2,14 @@
  * Problem link : https://cses.fi/problemset/task/1095
  * 
  * The naive solution would be to simply multiply the number a b-times.
- * But we can reduce the time complexity significantly from O(b) to O(log(b)) if notice
+ * But we can reduce the time complexity significantly from O(b) to O(log(b)) if we notice
  * that a^b equals:
  * ----> a^(b/2) * a^(b/2) when b is even
  * ----> a^(b/2) * a^(b/2) * b when b is odd
  * 
  * This tells us that if we know a^(b/2) we can simply take that number and multiply with itself
- * once to get a^b (or multiply again with b when b is odd). So we just need to know a^(b/2) and
- * analogously if we know a^(b/4) we can do the same thing to determine a^(b/2) and so on...
+ * once to get a^b (or multiply again with b when b is odd). So we just need to know a^(b/2).
+ * Nnalogously if we know a^(b/4) we can apply the same principle to determine a^(b/2) and so on...
  * You can see the pattern here.
  * The base case of this recursion is when b = 0: we can just return 1 because a^0 = 1.
  * 
